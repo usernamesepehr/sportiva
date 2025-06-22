8f8dc480c881202df225993f5a5c7b090c846692
sepehr <seeetayeshgholami85@gmail.com>
2025-06-22 04:42:59 -0700

    authenication system created

---
+++ app/Http/Controllers/authcontroller.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class authcontroller extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => ['required', 'min:4', 'max:16'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'phone' => ['required', 'min:10', 'max:15'],
+            'password' => ['required', 'min:8', 'regex:/^[a-zA-z0-9]+$/'],
+            'role' => ['required', 'integer']
+        ],[
+            'username.required' => 'وارد کردن نام کاربری الزامی است',
+            'username.min' => ' نام کاربری حداقل باید 4 حرف باشد ',
+            'username.max' => 'نام کاربری حداکثر باید 16 کرکتر باشد',
+            'email.required' => 'وارد کردن ایمیل الزامی میباشد',
+            'email.email' => 'ایمیل وارد شده معتبر نمیباشد',
+            'email.unique' => 'ایمیل مورد نظر قبلا وارد شده است',
+            'phone.required' => 'وارد کردن  شماره تلفن الزامی است',
+            'phone.min' => 'شماره تلفن حداقل باید10 کرکتر باشد',
+            'phone.max' => 'شماره تلفن حداکثر 15 کرکتر باشد',
+            'password.required' => 'وارد کردن رمز عبور الزامی است',
+            'password.min' => 'رمز عبور حداقل باید 8 کرکتر باشد',
+            'password.regex' => 'رمز عبور باید فقط شامل حروف و اعداد انگلیسی باشد'
+        ]);
+
+
+        
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+        
+        
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+
+        $customclaims = [
+           'id' => $user->id,
+           'role' => $user->role
+        ];
+
+        $token = JWTAuth::claims($customclaims)->fromUser($user);
+
+        
+
+        Auth::login($user);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User registered successfully',
+            'token' => $token,
+            'type' => 'bearer'
+        ]);
+
+    }
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+          'email' => ['required', 'email'],
+          'password' => ['required', 'min:8', 'regex:/^[a-zA-z0-9]+$/']
+        ], [
+           'email.required' => 'وارد کزدن ایمیل الزامی میباشد',
+           'email.email' => 'ایمیل وارد شده نامعتبر است',
+           'password.required' => 'وارد کردن رمز عبور الزامی میباشد',
+           'password.min' => 'رمز عبوز حداقل باید 8 کاراکتر باشد',
+           'password.regex' => 'رمز عبور باید فقط شامل حروف و اعداد انگلیسی باشد'
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json([
+               'status' => false,
+               'error' => $validator->errors()
+            ], 422);
+        }
+
+        if (!auth()->attempt($validator->validated()))
+        {
+              return response()->json([
+                 'status' => false,
+                 'message' => 'اطلاعات نامعتبر است'
+              ], 401);
+        }
+
+        $user = auth()->user();
+
+        $customClaims = [
+            'id' => $user->id,
+            'role' => $user->role
+        ];
+
+        $token = JWTAuth::claims($customClaims)->fromUser($user);
+        // $payload = JWTAuth::setToken($token)->getPayload();
+
+        return response()->json([
+           'status' => true,
+           'message' => 'ورود موفقیت امیز',
+           'token' => $token,
+           'type' => 'bearer'
+        ]);
+    }
+
+    public function logout()
+    {
+          Auth::logout();
+          return response()->json([
+            'status' => 'succes',
+            'message' => 'succesfully logged out'
+          ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\authcontroller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [authcontroller::class, 'register'])->middleware('throttle:register');
+Route::post('/login', [authcontroller::class, 'login'])->name('login')->middleware('throttle:register');
+Route::post('/logout', [authcontroller::class, 'logout'])->middleware('auth:api');
